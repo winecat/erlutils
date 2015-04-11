10ed8f58089e008fbf9faed0ca2dd3cb6e3d0519
mars <winecat@qq.gom>
2015-04-11 17:50:58 +0800

    add fun ip <=> integer

--- src/tools.erl
+++ src/tools.erl
@@ -48,6 +48,8 @@
          ,md5/1
          ,ip2bitstring/1
          ,ip2bitstring/4
+         ,ip_to_integer/1
+         ,integer_to_ip/1
          ,merge_kv/1
          ,calc_earth_distance/2
         ]).
@@ -451,6 +453,22 @@ ip2bitstring({N1, N2, N3, N4}) ->
 ip2bitstring(_) -> <<"">>.
 
 ip2bitstring(N1, N2, N3, N4) -> ip2bitstring({N1, N2, N3, N4}).
+
+%% @doc ip地址转成数值
+%% tips:
+%% "11111111,00000000,00000000" = integer_to_list(16711680,2).  
+%% "1111111100000000" = integer_to_list(65280,2).   
+%% "1000000000000000000000000" = integer_to_list(16777216,2).
+%% "10000000000000000" = integer_to_list(65536,2).   
+%% "100000000" = integer_to_list(256,2).  
+-spec ip_to_integer({N1 :: integer(), N2 :: integer(), N3 :: integer(), N4 :: integer()}) -> Result :: integer().
+ip_to_integer({N1, N2, N3, N4}) ->
+    (N1 * 16777216) + (N2 * 65536) + (N3 * 256) + N4.
+
+%% @doc 数值转成ip地址
+-spec integer_to_ip(Integer :: integer()) -> {N1 :: integer(), N2 :: integer(), N3 :: integer(), N4 :: integer()}.
+integer_to_ip(Integer) ->
+    {Integer bsr 24, (Integer band 16711680) bsr 16, (Integer band 65280) bsr 8, Integer band 255}.
 
 %% @doc 合并相同的键值对
 -spec merge_kv(List :: list({Key :: term(), Val :: integer()})) -> MergeList :: list().
