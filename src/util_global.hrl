b0b3b2879d200c2d30e28506eeef987d7a327658
winecat <winecat@qq.com>
2017-01-13 00:12:25 +0800

    mars
    新增 全局变量动态编译文件，改自mochiweb/mochiglobal.erl

---
+++ src/util_global.hrl
@@ -0,0 +1,8 @@
+%% error macro
+-ifndef(UTIL_GLOBAL_HRL).
+-define(UTIL_GLOBAL_HRL, ok).   %% UTIL_HRL START
+
+-define(UGL_TEST, ugl_test).	%% 测试key
+-define(UGL_OPEN_TIME, ugl_open_time).	%%开服时间
+
+-endif. %% UTIL_GLOBAL_HRL END
